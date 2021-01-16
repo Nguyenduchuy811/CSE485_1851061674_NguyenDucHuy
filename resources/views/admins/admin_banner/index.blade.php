d4d731e9dda44fc10930feabfad17bd81f56c51e
nanggat2018@gmail.com <nanggat2018@gmail.com>
2021-01-16 15:53:33 +0700

    hoa

--- resources/views/admins/admin_banner/index.blade.php
+++ resources/views/admins/admin_banner/index.blade.php
@@ -6,13 +6,7 @@
         <tr>
             <th>Stt</th>
             <th>Id</th>
-            <th>Username</th>
-            <th>Fisrtname</th>
-            <th>Lastname</th>
-            <th>Gender</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Address</th>
+            <th>Image banner</th>
             <th>Manipulation</th>
         </tr>
         </thead>
@@ -20,25 +14,15 @@
         
         <tbody>
         <?php
-        foreach ($users as $key => $value) { 
+        foreach ($banner as $key => $value) { 
             ?>
         <tr>
             <td scope="row"><?php echo $key+1;?></td>
             <td><?php echo $value["id"];?></td>
-            <td><?php echo $value["username"];?></td>
-            <td><?php echo $value["firstname"];?></td>
-            <td><?php echo $value["lastname"];?></td>
-            <td><?php if($value["gender"]==1){
-                    echo "Nam";
-                }else{
-                    echo "Nữ";
-                }?></td>
-            <td><?php echo $value["email"];?></td>
-            <td><?php echo $value["phone"];?></td>
-            <td><?php echo $value["address"];?></td>
+            <td><?php echo $value["image"];?></td>
             <td class="icon_admin">
-                <a href="<?php echo URL::to('/'); ?>/edit_user?id=<?php echo $value['id'];?>" class="fas fa-user-edit"></a>
-                <a href="<?php echo URL::to('/'); ?>/delete_user?id=<?php echo $value['id'] ?>" class="fas fa-trash-alt"
+                <a href="<?php echo URL::to('/'); ?>/edit_banner?id=<?php echo $value['id'];?>" class="fas fa-user-edit"></a>
+                <a href="<?php echo URL::to('/'); ?>/delete_banner?id=<?php echo $value['id'] ?>" class="fas fa-trash-alt"
                 onclick="return confirm('Are you sure want to delete?')"></a>
                 <a href="<?php echo URL::to('/'); ?>/info_user?id=<?php echo $value["id"];?>" class="far fa-question-circle"></a>
             </td>
